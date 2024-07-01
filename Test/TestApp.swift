06d7c2cb595dc7759514d3b9db34150d6a128047
Marek Michalski <marek.michalski@me.com>
2024-07-01 17:07:21 +0200

    Initial Commit

---
+++ Test/TestApp.swift
@@ -0,0 +1,17 @@
+//
+//  TestApp.swift
+//  Test
+//
+//  Created by Marek on 01/07/2024.
+//
+
+import SwiftUI
+
+@main
+struct TestApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
